e6bcea6b77facad7f381cbf01d1dda0fc0dfd970
Burak Güllüler <burakgulluler@gmail.com>
2022-11-24 20:45:10 +0300

    VowelCount update

--- src/_7_kyu/VowelCount.java
+++ src/_7_kyu/VowelCount.java
@@ -11,11 +11,16 @@ The input string will only consist of lower case letters and/or spaces.
 import java.util.ArrayList;
 import java.util.Arrays;
 
+
+
 public class VowelCount {
+    public static void main(String[] args) {
+        System.out.println(getCount("aass"));
+    }
     public static int getCount(String str) {
+        String x = "asdasd";
         int result = 0;
-        ArrayList<Character> vowels = new ArrayList<>();
-        vowels.addAll(Arrays.asList('a','e','i','o','u'));
+        var vowels = Arrays.asList('a','e','i','o','u');
         for (int i = 0; i<str.length(); i++){
             if (vowels.contains(str.charAt(i))){
                 result++;
