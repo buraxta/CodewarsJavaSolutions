3bda5aa446b882cd5769dac6b790834c9eff7c92
Burak Güllüler <burakgulluler@gmail.com>
2022-11-27 08:41:30 +0300

    Valid_Parentheses

--- src/_5_kyu/Valid_Parentheses.java
+++ src/_5_kyu/Valid_Parentheses.java
@@ -1,7 +1,22 @@
 package _5_kyu;
 
 import java.util.Stack;
+/*
+Write a function that takes a string of parentheses, and determines if the order of the parentheses is valid.
+The function should return true if the string is valid, and false if it's invalid.
 
+Examples
+"()"              =>  true
+")(()))"          =>  false
+"("               =>  false
+"(())((()())())"  =>  true
+Constraints
+0 <= input.length <= 100
+
+Along with opening (() and closing ()) parenthesis, input may contain any valid ASCII characters.
+Furthermore, the input string may be empty and/or not contain any parentheses at all.
+Do not treat other forms of brackets as parentheses (e.g. [], {}, <>).
+*/
 public class Valid_Parentheses {
     public static boolean validParentheses(String parens)
     {
